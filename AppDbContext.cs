347fc064812d9dea19192986a880468dad219e27
Carita <caritadm@hotmail.com>
2024-08-15 16:48:18 -0300

    Adicionar arquivos de projeto.

---
+++ AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NunesSportsAPI.Models;
+
+namespace NunesSportsAPI
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+
+    }
+}
---
+++ Models/Product.cs
@@ -0,0 +1,21 @@
+﻿namespace NunesSportsAPI.Models
+{
+using System.ComponentModel.DataAnnotations;
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public string Codigo { get; set; }
+
+        public string Descricao { get; set; }
+
+        [Required]
+        public decimal Preco { get; set; }
+
+    }
+}
